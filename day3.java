f4d4a45bf7650502b057ee4fc8687e2dcfd3eb9f
piyush26832 <piyushayushsharma12345@gmail.com>
2026-01-28 10:08:52 +0530

    Update day3 logic

--- day3.java
+++ day3.java
@@ -1,8 +1,16 @@
+public class day3
+ {
+    public static void main(String[] args) {
+        int[] arr = {3, 7, 2, 9, 4};
 
-import java.net.http.WebSocketHandshakeException;
+        int max = arr[0]; // assume first element is max
 
-public class day3 {
-    // jbfjqbqjkbfqfqjkjknaefknK
-    WebSocketHandshakeExceptionkwkm
-    kwkww
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum element is: " + max);
+    }
 }
